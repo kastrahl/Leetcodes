e2a42c7cbe1ac17c3715a467c59f10a4ac28a5ab
kastrahl <56175823+kastrahl@users.noreply.github.com>
2021-04-08 05:29:27 +0530

    Update 21.c
    the solution accepted

--- 21.c
+++ 21.c
@@ -1,118 +1,91 @@
-#include <stdio.h>
-#include <stdlib.h>
-struct Node {
-    int data;
-    struct Node* next;
-};                      //no problems
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
 
-void printList(struct Node* n){
-    while (n != NULL) {
-        printf(" %d ", n->data);
-        n = n->next;
-    }
-    printf("\n");
-}     //no problems
 
-void insert_given_node(struct Node* l1, int afterdata, int datatoinsert) 
+void append(struct ListNode** head_ref, int new_data)
 {
-    struct Node *ptr, *cpt;
-    int data;
-    ptr = (struct Node*)malloc(sizeof(struct Node));
-    if (ptr == NULL) 
+    struct ListNode *new_node = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode *last = *head_ref;
+    new_node->val  = new_data;
+    new_node->next = NULL;
+    if (*head_ref == NULL)
     {
-        printf ("overflow\n");
-        return;
-    }
-    
-    ptr->data = datatoinsert;
-    
-    cpt = l1;
-    while (cpt -> data != afterdata)
-    cpt = cpt -> next;
-    ptr -> next = cpt -> next;
-    cpt -> next = ptr;
+       *head_ref = new_node;
+       return;
+    }      
+    while (last->next != NULL)
+        last = last->next;
+    last->next = new_node;
+    return;    
 }
 
-struct Node* mergeTwoLists(struct Node* l1, struct Node* l2)
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2)
 {
+    
+    if(l1 == NULL&&l2 != NULL)
+    {
+        return l2;
+    }
+    else
+    if(l1 != NULL&&l2 == NULL)
+    {
+        return l1;
+    }
+    else
     if(l1 == NULL&&l2 == NULL)
     {
         return l1;
     }
     
-    int data;
-    struct Node *t1,*t2,*ptr,*start=l1;                       //t1= l1's next node , t2= l2's current position's data
-    printList(start);
-    
-    ptr=(struct Node*)malloc(sizeof(struct Node));
-    
-    while(l1!=NULL)
+    if(l1->val > l2->val)
     {
-        if(l1->next!=NULL)                          //check if not last node in l1
-            {
-                t1=l1->next;                        //store L1'S NEXT NODE in t1 to compare with l2's current node
-                if(t1->data <= l2->data)            //if l1's next node's data is less or equal to l2's current node
-                {
-                    l1=l1->next;                    //proceed to next l1's next node ( the node in l1 is less than l2)
-                }
-                else
-                {
-                    insert_given_node(l1,l1->data,l2->data);
-                    l1=l1->next;
-                    l2=l2->next;
-                }
-            }
-        else
-            {
-                l1->next=l2;
-                l1=l1->next;
-                break;
-            }
+        struct ListNode *temp;
+        temp=l1;
+        l1=l2;
+        l2=temp;
     }
-    printList(start);
-    return start;
-}
-
-int main()  //no problems
-{
-    struct Node* head = NULL;
-    struct Node* second = NULL;
-    struct Node* third = NULL;
+    int data;
+    struct ListNode *start=l1;                       //t1= l1's next node , t2= l2's current position's data
+    struct ListNode *t1= NULL; 
     
-    head = (struct Node*)malloc(sizeof(struct Node));
-    second = (struct Node*)malloc(sizeof(struct Node));
-    third = (struct Node*)malloc(sizeof(struct Node));
-    
-    head->data = 1; 
-    head->next = second;
-    
-    second->data = 2; 
-    second->next = third;
-    
-    third->data = 3; 
-    third->next = NULL;
-    
-    printList(head);
-    
-    struct Node* h = NULL;
-    struct Node* s = NULL;
-    struct Node* t = NULL;
-    
-    h = (struct Node*)malloc(sizeof(struct Node));
-    s = (struct Node*)malloc(sizeof(struct Node));
-    t = (struct Node*)malloc(sizeof(struct Node));
-    
-    h->data = 1; 
-    h->next = s;
-    
-    s->data = 2; 
-    s->next = t;
-    
-    t->data = 4; 
-    t->next = NULL;
-    
-    printList(h);
-    
-    mergeTwoLists(head,h);
-
+    while(l1!=NULL||l2!=NULL)
+    {
+        if(l1==NULL)
+        {
+            while(l2!=NULL)
+            {
+                append(&t1,l2->val);
+                l2=l2->next;
+            }
+        }
+        else 
+        if(l2==NULL)
+        {
+            while(l1!=NULL)
+            {
+                append(&t1,l1->val);
+                l1=l1->next;
+            }
+        }
+        
+        if(l1!=NULL && l1->val <= l2->val)
+        {
+            data = l1->val;
+            append(&t1,l1->val);
+            l1=l1->next;
+        }
+        else
+        if(l2!=NULL && l1->val > l2->val )
+        {
+            data=l2->val;
+            append(&t1,l2->val);
+            l2=l2->next;
+        }
+    }
+    return t1;
 }
